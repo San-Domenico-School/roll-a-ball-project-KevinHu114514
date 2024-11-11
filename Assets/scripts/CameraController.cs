3d0e3b26f7d57f25347d2b2bdd71046fc5c1011e
KevinHu114514 <khu28@sandomenico.org>
2024-11-11 15:08:05 -0800

    11.11 process

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+    [SerializeField] private Transform player;
+    private float deltaY = 1f;
+    [SerializeField] private float rotationSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        RotateCamera();
     }
-}
+
+    void RotateCamera()
+    {
+
+        // Get mouse input
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+
+        //Calculate roate angle
+        Vector3 playerPosition = player.position + new Vector3(0, deltaY, 0);
+        transform.Rotate(Vector3.up, mouseX * rotationSpeed);
+
+    }
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;        // The target the camera should follow
+        public float smoothSpeed = 0.125f; // How smooth the camera movement will be
+        public Vector3 offset;         // The offset of the camera relative to the target
+
+        void FixedUpdate()
+        {
+            // Calculate the desired position with offset
+            Vector3 desiredPosition = target.position + offset;
+
+            // Smoothly move towards the desired position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            // Update camera position
+            transform.position = smoothedPosition;
+
+            // Optionally, make the camera always look at the target
+            transform.LookAt(target);
+        }
+    }
+
+ }
+
