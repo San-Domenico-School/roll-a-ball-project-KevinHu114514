95b31c2a16eda639fe6d5b70a1b64804414467da
KevinHu114514 <khu28@sandomenico.org>
2024-11-15 15:00:07 -0800

    11.15 process

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -5,25 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    private float deltaY = 1f;
-    [SerializeField] private float rotationSpeed;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        RotateCamera();
+        FollowPlayer();
     }
 
-    void RotateCamera()
+    void FollowPlayer()
     {
 
-        // Get mouse input
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-
-        //Calculate roate angle
-        Vector3 playerPosition = player.position + new Vector3(0, deltaY, 0);
-        transform.Rotate(Vector3.up, mouseX * rotationSpeed);
-
+        transform.position = player.position + offset;
     }
     
 
